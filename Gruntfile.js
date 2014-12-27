c750deb9ceb0c6443888a77fe9292ee642382600
Ritesh Kumar <rkritesh078@gmail.com>
2014-12-28 00:12:24 +0530

    build task corrected

--- Gruntfile.js
+++ Gruntfile.js
@@ -209,9 +209,7 @@ module.exports = function (grunt) {
           src: [
             '<%= config.dist %>/scripts/{,*/}*.js',
             '<%= config.dist %>/styles/{,*/}*.css',
-            '<%= config.dist %>/images/{,*/}*.*',
-            '<%= config.dist %>/styles/fonts/{,*/}*.*',
-            '<%= config.dist %>/*.{ico,png}'
+            '<%= config.dist %>/fonts/{,*/}*.*'
           ]
         }
       }
